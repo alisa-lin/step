bacfecb40f29b542082fc1e8873a47258386794c
Alisa Lin <alisalin610@gmail.com>
2020-06-10 17:53:11 +0000

    add chart

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,3 +11,27 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+/** Creates a chart and adds it to the page. */
+function drawChart() {
+    console.log("drawing chart");
+    var data = google.visualization.arrayToDataTable([
+        ['App', 'Hours per Week'],
+        ['Instagram',     6],
+        ['Facebook',      4.8],
+        ['Reddit',  4.6],
+        ['Messenger', 3.75],
+        ['Youtube',    3.25]
+    ]);
+
+    var options = {
+        title: 'My Screentime',
+        pieHole: 0.4,
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('chart-container'));
+    chart.draw(data, options);
+}
